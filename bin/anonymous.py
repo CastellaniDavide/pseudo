6c2b304ffaabd8eaf11e451b126fa7b5a1aa0662
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-01-19 10:59:03 +0100

    alfa version (v1.0)

--- bin/anonymous.py
+++ bin/anonymous.py
@@ -2,18 +2,53 @@
 """
 
 __author__ = "help@castellanidavide.it"
-__version__ = "1.0 2021-1-19"
+__version__ = "1.0 2021-01-19"
 
 class anonymous:
-	def __init__ (self):
+	def __init__ (self, div=";"):
 		"""Where it all begins
 		"""
-		print(anonymous.anonymous())
+		self.div = div
+		self.read_input()
+		self.get_positions()
+		self.write_output()
 	
-	def anonymous():
-		"""anonymous first funtion
+	def read_input(self):
+		"""Reads my input file(s)
 		"""
-		return "anonymous" #for the test (see test_anonymous.py)
+		self.conf = open("../flussi/anonymus.conf").read().split("\n")
+
+		first = True
+		self.body = []
+
+		for line in open("../flussi/example.csv"):
+			line = line.strip().split(self.div)
+			if first:
+				first = False
+				self.header = line
+			else:
+				self.body.append(line)
+
+	def get_positions(self):
+		"""Gets the positions
+		"""
+		self.positions = []
+		for element in self.conf:
+			self.positions.append(self.header.index(element))
+
+	def write_output(self):
+		"""Write my outputs
+		"""
+		file_out = open("../flussi/file_out.csv", "w+")
+		for j in sorted(self.positions, reverse=True):
+			del self.header[j] 
+		file_out.write(f'{self.header}\n')
+		for i in self.body:
+			for j in sorted(self.positions, reverse=True):
+				del i[j] 
+			file_out.write(str(i))
+			file_out.write("\n")
+		file_out.close()
 		
 if __name__ == "__main__":
 	anonymous()
