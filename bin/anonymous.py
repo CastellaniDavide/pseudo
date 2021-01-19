18a520c34a62ff3191506571ef4eed15c02b825b
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-01-19 09:36:47 +0100

    Created bin/anonymous.py

---
+++ bin/anonymous.py
@@ -0,0 +1,19 @@
+"""anonymous
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-1-19"
+
+class anonymous:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(anonymous.anonymous())
+	
+	def anonymous():
+		"""anonymous first funtion
+		"""
+		return "anonymous" #for the test (see test_anonymous.py)
+		
+if __name__ == "__main__":
+	anonymous()
